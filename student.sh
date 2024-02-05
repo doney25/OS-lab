a3aceebd7d1b6dcedc0601b9b72cac9623a9520a
Doney Siby <145443681+doney25@users.noreply.github.com>
2024-02-05 10:30:01 +0530

    student details using shell

---
+++ student.sh
@@ -0,0 +1,12 @@
+echo "Enter the name"
+read name
+echo "Enter the Semester"
+read sem
+echo "Enter the batch"
+read batch
+echo "Enter the roll no:"
+read roll_no
+echo "Enter the register no:"
+read reg
+echo -e "Name is $name\nSemester is $sem\nBatch is $batch\nRoll no is $roll_no\nRegister number is $reg"
+
